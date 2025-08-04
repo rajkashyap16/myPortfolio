da808380f32b8ecde505809834334f43aed3cb1a
Raj kashyap <78655730+rajkashyap16@users.noreply.github.com>
2025-08-04 15:57:09 +0530

    Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-<<<<<<< HEAD
+
 import profile from "../assets/profile.jpg";
 import down from "../assets/down2.svg";
-=======
 
->>>>>>> 0f789d2045182d0e53901bcc3b16fc0d18784c32
 
 const Hero = () => {
   return (
@@ -16,7 +14,7 @@ const Hero = () => {
         This is a portfolio website that showcases my skills and experience as a
         web developer.
       </p>
-<<<<<<< HEAD
+
       <img
         src={profile}
         alt="image"
@@ -26,7 +24,7 @@ const Hero = () => {
         {/* cursor icon that send to project section that uses id to navigate to that section */}
         <a href="#projects">
           <img src={down} className="text-white animate-bounce " width={50} />
-=======
+
       <img src="../assets/profile.jpg"  alt="image" className="w-48 mx-auto rounded-full object-cover border m-4"  />
       <button className=" text-white font-thin  my-[8%] mx-auto">
         {/* cursor icon that send to project section that uses id to navigate to that section */}
@@ -37,7 +35,7 @@ const Hero = () => {
             width={50}
         
           />
->>>>>>> 0f789d2045182d0e53901bcc3b16fc0d18784c32
+
         </a>
       </button>
     </div>
